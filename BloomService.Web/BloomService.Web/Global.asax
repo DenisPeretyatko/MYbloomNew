d3c2c3263002c7ac87b798cb30695796101c7161
DenisPeretyatko <denis.peretyatko@iquesoft.net>
2016-03-29 12:13:17 +0300

    Initial commit

---
+++ BloomService.Web/BloomService.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BloomService.Web.MvcApplication" Language="C#" %>
---
+++ BloomService.Web/BloomService.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BloomService.Web.Startup))]
+namespace BloomService.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
