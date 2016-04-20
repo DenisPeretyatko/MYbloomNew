bd5d1c5667720d89eac8f83a88a1aee58c727a16
DenisPeretyatko <denis.peretyatko@iquesoft.net>
2016-04-20 12:16:58 +0300

    change login end point for IOS

--- BloomService.Web/App/workorders/editWorkorderController.js
+++ BloomService.Web/App/workorders/editWorkorderController.js
@@ -19,17 +19,19 @@ var editWorkorderController = function ($scope, $stateParams, commonDataService,
     $scope.permissiocode = '';
     $scope.paymentmethods = '';
     $scope.lookups = state.lookups;
+    $scope.editableWorkOrder = {}
 
     $scope.$watch(function () { return state.lookups; }, function () {
         $scope.lookups = state.lookups;
-        $scope.lookups.Customers.selected = $scope.editableWorkOrder.ARCustomer;
-        $scope.lookups.Locations.selected = $scope.editableWorkOrder.Location;
-        $scope.calltype = $scope.editableWorkOrder.CallType;
+
+        $scope.lookups.Customers.selected = { Name: $scope.editableWorkOrder.ARCustomer };
+        $scope.lookups.Locations.selected = { Name: $scope.editableWorkOrder.Location };
+        $scope.lookups.Calltypes.selected = { Description: $scope.editableWorkOrder.CallType };
         $scope.calldate = $scope.editableWorkOrder.CallDate;
-        $scope.lookups.Problems.selected = $scope.editableWorkOrder.Problem;
+        $scope.lookups.Problems.selected = { Description: $scope.editableWorkOrder.Problem };
         $scope.lookups.RateSheets.selected = $scope.editableWorkOrder.RateSheet;
-        $scope.lookups.Employes.selected = $scope.editableWorkOrder.Employee;
-        $scope.lookups.Equipment.selected = $scope.editableWorkOrder.Equipment;
+        $scope.lookups.Employes.selected = { Name: $scope.editableWorkOrder.Employee };
+        $scope.lookups.Equipment.selected = { EquipmentType: $scope.editableWorkOrder.Equipment };
         //$scope.lookups.Hours.selected = $scope.editableWorkOrder.Contact;
         $scope.nottoexceed = $scope.editableWorkOrder.NottoExceed;
         $scope.locationcomments = $scope.editableWorkOrder.Area;
@@ -43,8 +45,9 @@ var editWorkorderController = function ($scope, $stateParams, commonDataService,
 	};
 
 	commonDataService.getWorkorder($stateParams.id).then(function (response) {
-	    return $scope.editableWorkOrder = response.data;
+	    $scope.editableWorkOrder = response.data;
 	});
+
 
  
 
--- BloomService.Web/Areas/Apimobile/Controllers/AuthorizationController.cs
+++ BloomService.Web/Areas/Apimobile/Controllers/AuthorizationController.cs
@@ -7,7 +7,9 @@ namespace BloomService.Web.Areas.Apimobile.Controllers
     {
         public string Get(string name, string password)
         {
-            return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWVfbmFtZSI6ImtyaXMiLCJmYW1pbHlfbmFtZSI6InNhZ2VERVYhISIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTI0NDIiLCJhdWQiOiI0MTRlMTkyN2EzODg0ZjY4YWJjNzlmNzI4MzgzN2ZkMSIsImV4cCI6MTQ2MTA3MDA4MywibmJmIjoxNDYwOTgzNjgzfQ.RUpXvqnam1_0HSl6SUrhFSakq2187EA7g7ASZeKKyxU";
+            if(name == "kris" && password=="111")
+                return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWVfbmFtZSI6ImtyaXMiLCJmYW1pbHlfbmFtZSI6InNhZ2VERVYhISIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTI0NDIiLCJhdWQiOiI0MTRlMTkyN2EzODg0ZjY4YWJjNzlmNzI4MzgzN2ZkMSIsImV4cCI6MTQ2MTA3MDA4MywibmJmIjoxNDYwOTgzNjgzfQ.RUpXvqnam1_0HSl6SUrhFSakq2187EA7g7ASZeKKyxU";
+            return "Login failed";
         }
     }
 }
