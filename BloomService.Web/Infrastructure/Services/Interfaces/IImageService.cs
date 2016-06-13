a831a32cb871c47d8e993b56a32ac8522df75744
Alexey Kharchenko <Lyosha1992@gmail.com>
2016-06-13 15:28:29 +0300

    Change build icon technicians and workorders

--- BloomService.Web/Infrastructure/Services/Interfaces/IImageService.cs
+++ BloomService.Web/Infrastructure/Services/Interfaces/IImageService.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using BloomService.Web.Models;
+using System.Drawing;
 
 namespace BloomService.Web.Services.Abstract
 {
@@ -6,5 +7,6 @@ namespace BloomService.Web.Services.Abstract
     {
         string SaveFile(string file, string path, string userId);
         bool CreateIcon(string nameIcon, string color, string id, Color oldColor);
+        bool BuildTechnicianColor(TechnicianModel technician);
     }
 }
