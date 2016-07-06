12a114db26de08f1cf6c5b7d7d1cddc382bfcfdf
Kirill Tairov <pyro.sun@gmail.com>
2016-07-06 17:13:35 +0300

    Fix web.config and fix workorder`s item collection

--- BloomService.Web/Controllers/ApiMobileController.cs
+++ BloomService.Web/Controllers/ApiMobileController.cs
@@ -170,7 +170,7 @@ namespace BloomService.Web.Controllers
                     var equipments = repository.SearchFor<SageEquipment>(x => x.Equipment == order.Equipment.ToString());
                     order.Equipments.AddRange(equipments);
                 }
-                order.WorkOrderItems = repository.SearchFor<SageWorkOrderItem>(x => x.WorkOrder.ToString() == order.WorkOrder).ToList();//repository.GetAll<SageWorkOrderItem>().ToList().Where(x => x.WorkOrder.ToString() == order.WorkOrder);
+                order.WorkOrderItems = repository.SearchFor<SageWorkOrderItem>().ToList().Where(x => x.WorkOrder.ToString() == order.WorkOrder);
             }
 
             return Json(result, JsonRequestBehavior.AllowGet);
