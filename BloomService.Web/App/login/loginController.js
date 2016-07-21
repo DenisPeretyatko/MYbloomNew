b71d89f0fb13b4793d09a7cbb536855e6a5eb668
Kirill Tairov <pyro.sun@gmail.com>
2016-07-21 13:24:23 +0300

    fix locations and lookups loading

--- BloomService.Web/App/login/loginController.js
+++ BloomService.Web/App/login/loginController.js
@@ -2,63 +2,47 @@
  * loginController - controller
  */
 
-var loginController = function($scope, $rootScope, $state, commonDataService, commonHub, state) {
+var loginController = function($scope, $rootScope, $state, commonDataService, commonHub, state, $q) {
     $scope.obj = {};
     window.localStorage.setItem('UserName', '')
     window.localStorage.setItem('Token', '');
     $scope.obj.username = 'testofficeuser';
     $scope.obj.password = 'sageDEV!!';
     $scope.obj.message = '';
+    var paginationModel = {
+        Index: 0,
+        Search: '',
+        Column: '',
+        Direction: true
+    }
     $scope.login = (function () {
-        commonDataService.getToken($scope.obj.username, $scope.obj.password).success(function (response) {
+        commonDataService.getToken($scope.obj.username, $scope.obj.password).success(function(response) {
             if (response.access_token != null && response.access_token != "" && response.access_token != undefined) {
                 window.localStorage.setItem('Token', response.access_token);
                 commonHub.LoginHub();
-                
-             
-                if (state.notificationTime == undefined || state.lookups == undefined || state.notifications == undefined) {
-                    commonDataService.getLookups().then(function(response) {
-                        $rootScope.notifications = response.data.Notifications;
-                        state.notifications = response.data.Notifications;
-                        state.notificationTime = response.data.NotificationTime;
-                        state.lookups = response.data;
-                    });
-                }
 
-                if (state.locations == undefined) {
-                    commonDataService.getLocations().then(function(response) {
-                        state.locations = response.data;
+                debugger;
+                if (state.alreadyLoaded == false ) { //|| state.notificationTime == undefined || state.lookups == undefined || state.notifications == undefined
+                    $q.all([commonDataService.getLookups(), commonDataService.getWorkordesPaged(paginationModel), commonDataService.getTechnicians(), commonDataService.getTrucks()]).then(function (values) { //, commonDataService.getLocations(),
+                       
+                        $rootScope.notifications = values[0].data.Notifications;
+                        state.notifications = values[0].data.Notifications;
+                        state.notificationTime = values[0].data.NotificationTime;
+                        state.lookups = values[0].data;
+                        //state.locations = values[1].data;
+                        state.workorders = values[1].data;
+                        state.technicians = values[2].data;
+                        $rootScope.trucks = values[3].data;
+                        state.trucks = values[3].data;
+                        $state.go('manager.dashboard');
                     });
-                }
-                var paginationModel = {
-                    Index: 0,
-                    Search: '',
-                    Column: '',
-                    Direction: true
-                };
-                if (state.workorders == undefined) {
-                    commonDataService.getWorkordesPaged(paginationModel).then(function(response) {
-                        state.workorders = response.data;
-                    });
-                }
-                if (state.technicians == undefined) {
-                    commonDataService.getTechnicians().then(function(response) {
-                        state.technicians = response.data;
-                    });
-                }
-                if (state.trucks == undefined) {
-                    commonDataService.getTrucks().then(function(response) {
-                        $rootScope.trucks = response.data;
-                        state.trucks = response.data;
-                    });
-                }
-
-                $state.go('manager.dashboard');
+                } 
+                else  $state.go('manager.dashboard');
             }
             else {
                 $scope.obj.message = 'Incorrect login or password';
-            }
-        })
+            };
+        });
     });
 };
-loginController.$inject = ["$scope", "$rootScope", "$state", "commonDataService", "commonHub", "state"];
+loginController.$inject = ["$scope", "$rootScope", "$state", "commonDataService", "commonHub", "state", "$q"];
