6d64ef6be7003aa34c16ff5b37df43728ebbf065
Kirill Tairov <pyro.sun@gmail.com>
2016-07-26 15:43:35 +0300

    add migrations

---
+++ BloomService.Migrations/Migrations/MigrationInitial.cs
@@ -0,0 +1,16 @@
+﻿using MongoMigrations;
+
+namespace BloomService.Migrations.Migrations
+{
+    public class MigrationInitial : Migration
+    {
+        public MigrationInitial() : base("0.0.0")
+        {
+
+        }
+        public override void Update()
+        {
+            //Database.Eval(new BsonJavaScript("db.WorkOrderCollection.createIndex( { WorkOrder:1, ScheduleDate: 1, DateEntered: 1, ARCustomer: 1, Location: 1, Status: 1 } );"));
+        }
+    }
+}
---
+++ BloomService.Web/App_Start/MongoMigrations.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Web;
+using BloomService.Migrations.Migrations;
+using MongoMigrations;
+
+[assembly: PreApplicationStartMethod(typeof(BloomService.Web.MongoMigrations), "Start")]
+
+namespace BloomService.Web
+{
+    public class MongoMigrations
+    {
+        public static void Start()
+        {
+            var connection = ConfigurationManager.ConnectionStrings["MongoServerSettings"].ConnectionString;
+            var dbName = ConfigurationManager.AppSettings["MainDb"];
+            var runner = new MigrationRunner(connection, dbName); 
+            runner.MigrationLocator.LookForMigrationsInAssemblyOfType<MigrationInitial>();
+            runner.DatabaseStatus.ThrowIfNotLatestVersion();
+            runner.MigrationLocator.GetAllMigrations();
+            runner.UpdateToLatest();
+        }
+    }
+}
