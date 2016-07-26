ffd9f07cc58a6a834e6c856e650c0069172d9637
Kirill Tairov <pyro.sun@gmail.com>
2016-07-26 17:47:10 +0300

    fix migrations

--- BloomService.Migrations/Migrations/MigrationAddMoreMemory.cs
+++ BloomService.Migrations/Migrations/MigrationAddMoreMemory.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoMigrations;
 
 namespace BloomService.Migrations.Migrations
 {
-    public class MigrationAddMoreMemory_20160726160929 : Migration
+    public class MigrationAddMoreMemory : Migration
     {
-        public MigrationAddMoreMemory_20160726160929() : base("0.0.6")
+        public MigrationAddMoreMemory() : base("0.0.1")
         {
 
         }
