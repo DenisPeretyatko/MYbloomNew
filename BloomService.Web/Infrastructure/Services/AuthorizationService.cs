3a46b7b849faed21e8df4320892188a36e9fdd6c
DenisPeretyatko <denis.peretyatko@iquesoft.net>
2016-08-09 08:00:37 -0700

    fix beta authorization

--- BloomService.Web/Infrastructure/Services/AuthorizationService.cs
+++ BloomService.Web/Infrastructure/Services/AuthorizationService.cs
@@ -52,11 +52,12 @@
 
         private AuthorizationResponse CheckUser(string name, string password)
         {
+            var apiHost = _configuration.SageApiHost == "http://12.217.205.13:82/" ? "http://12.217.205.13:81/" : _configuration.SageApiHost;
             var request = new RestRequest(EndPoints.Authorization, Method.POST) { RequestFormat = DataFormat.Json };
             var requestBody = new AuthorizationRequest() { Name = name, Password = password };
             request.AddBody(requestBody);
             request.AddHeader("Authorization", string.Format("Basic {0}:{1}", _configuration.SageUsername, _configuration.SagePassword));
-            var restClient = new RestClient(_configuration.SageApiHost);
+            var restClient = new RestClient(apiHost);
             var response = restClient.Execute<AuthorizationResponse>(request);
             return response.StatusCode != System.Net.HttpStatusCode.OK ? null
                 : new JavaScriptSerializer().Deserialize<AuthorizationResponse>(response.Content);
