08ec0c2eec1eb4383fc8f8b20addd1aa8a4fce48
Kirill Tairov <pyro.sun@gmail.com>
2016-08-23 13:28:02 +0300

    fix loading bar

---
+++ BloomService.Web/App/common/commonLoadingBarService.js
@@ -0,0 +1,36 @@
+﻿var loadingBarService = function($q, $rootScope) {
+    var requests = 0;
+    function show() {
+        if (!requests) {
+            $rootScope.$broadcast("ajax-start");
+        }
+        requests++;
+    }
+    function hide() {
+        requests--;
+        if (!requests) {
+            $rootScope.$broadcast("ajax-stop");
+        }
+    }
+    return {
+        'request': function (config) {
+            show();
+            return $q.when(config);
+        }, 'response': function (response) {
+            hide();
+            return $q.when(response);
+        }, 'responseError': function (rejection) {
+            hide();
+            return $q.reject(rejection);
+        }
+    };
+}
+
+loadingBarService.$inject = ["$q", "$rootScope"];
+
+
+var loadingBarConfig = function($httpProvider) {
+    $httpProvider.interceptors.push(loadingBarService);
+}
+
+loadingBarConfig.$inject = ["$httpProvider"];
