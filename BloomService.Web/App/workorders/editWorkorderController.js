151f53fbe0e5ed8fc73ba784882b486f49c64613
Kirill Tairov <pyro.sun@gmail.com>
2016-08-23 12:51:46 +0300

    add ability to edit comments on web ui

--- BloomService.Web/App/workorders/editWorkorderController.js
+++ BloomService.Web/App/workorders/editWorkorderController.js
@@ -396,5 +396,17 @@ var editWorkorderController = function ($scope, $rootScope, $stateParams, $state
         $scope.obj.hours = parseFloat(selected.$select.selected.EstimatedRepairHours);
     };
 
+    $scope.saveComment = function(comment, wo, id) {
+        var model = {
+            comment: comment,
+            workOrder: wo,
+            id: id
+        };
+        commonDataService.editComment(model).then(function (response) {
+            if (response.data.success == false)
+                alert("Failed to edit comment");
+        });
+    }
+
 };
 editWorkorderController.$inject = ["$scope", "$rootScope", "$stateParams", "$state", "$compile", "$interpolate", "commonDataService", "state"];
