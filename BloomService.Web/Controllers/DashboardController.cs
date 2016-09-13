b3eeb57c04bcb8a655eb6a33059eac91ea1e5635
Kirill Tairov <pyro.sun@gmail.com>
2016-09-13 11:28:02 +0300

    fix get dashboard

--- BloomService.Web/Controllers/DashboardController.cs
+++ BloomService.Web/Controllers/DashboardController.cs
@@ -71,8 +71,8 @@ namespace BloomService.Web.Controllers
         public ActionResult GetDashboard(MapModel model)
         {
             var dashboard = new DashboardViewModel();
-            var workorders = _repository.SearchFor<SageWorkOrder>().Open().ToArray();
-            var assignments = _repository.SearchFor<SageAssignment>(x => x.Employee == "").ToArray();
+            var workorders = _repository.SearchFor<SageWorkOrder>().Open().ToList();
+            var assignments = _repository.SearchFor<SageAssignment>(x => x.Employee == "").ToList();
             var chart = new List<ChartModel>();
             var chartModel = new ChartModel();
 
